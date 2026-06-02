86c92bada62fba3036b4d7e77200330bbab73166
zhangting2020 <zhangting_2017@163.com>
2026-06-02 23:24:58 +0800

    Apply VMM V1 free hot path fix

--- paddle/phi/core/memory/allocation/virtual_memory_auto_growth_best_fit_allocator.cc
+++ paddle/phi/core/memory/allocation/virtual_memory_auto_growth_best_fit_allocator.cc
@@ -201,14 +201,7 @@ phi::Allocation *VirtualMemoryAutoGrowthBestFitAllocator::AllocateImpl(
 void VirtualMemoryAutoGrowthBestFitAllocator::FreeImpl(
     phi::Allocation *allocation) {
   std::lock_guard<SpinLock> guard(spinlock_);
-  void *ptr = allocation->ptr();
-  auto block_it = FindBlockByPtr(ptr);
-  if (block_it == all_blocks_.end()) {
-    VLOG(4) << "[VMM][FreeImplMissingBlock] ptr=" << ptr
-            << " allocation_size=" << allocation->size();
-    delete allocation;
-    return;
-  }
+  auto block_it = static_cast<BlockAllocation *>(allocation)->block_it_;
   TryMergeBlock2Blocks(block_it);
   delete allocation;
 }
@@ -240,14 +233,6 @@ bool VirtualMemoryAutoGrowthBestFitAllocator::CollectTensorParts(
     }
   }
   return false;
-}
-
-std::list<Block>::iterator
-VirtualMemoryAutoGrowthBestFitAllocator::FindBlockByPtr(void *ptr) {
-  for (auto it = all_blocks_.begin(); it != all_blocks_.end(); ++it) {
-    if (it->ptr_ == ptr) return it;
-  }
-  return all_blocks_.end();
 }
 
 void VirtualMemoryAutoGrowthBestFitAllocator::TryMergeBlock2Blocks(
