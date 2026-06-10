b10395e1db5e7fa7a2c730d5b55f23b85459e10a
zhangting2020 <zhangting_2017@163.com>
2026-06-10 12:09:24 +0800

    Record VMM V2 remap safety event on free

--- paddle/phi/core/memory/allocation/stream_safe_cuda_allocator.cc
+++ paddle/phi/core/memory/allocation/stream_safe_cuda_allocator.cc
@@ -178,7 +178,19 @@ bool StreamSafeCUDAAllocation::SetVMMV2RemapEvent() {
   if (vmm_v2_remap_allocation_ == nullptr) {
     return false;
   }
+  if (!FLAGS_vmm_v2_remap_on_oom) {
+    return vmm_v2_remap_allocation_->SetVMMRemapEvent(owning_stream_, nullptr);
+  }
+#if defined(PADDLE_WITH_CUDA)
+  gpuEvent_t remap_event = nullptr;
+  PADDLE_ENFORCE_GPU_SUCCESS(
+      cudaEventCreateWithFlags(&remap_event, cudaEventDisableTiming));
+  PADDLE_ENFORCE_GPU_SUCCESS(cudaEventRecord(remap_event, owning_stream_));
+  return vmm_v2_remap_allocation_->SetVMMRemapEvent(
+      owning_stream_, std::make_shared<CUDAEventGuard>(remap_event));
+#else
   return vmm_v2_remap_allocation_->SetVMMRemapEvent(owning_stream_, nullptr);
+#endif
 }
 
 void StreamSafeCUDAAllocation::RecordStreamWithNoGraphCapturing(
